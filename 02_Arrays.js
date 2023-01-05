b5d9b51cafe8d07e3afba1f897f07d912992b0ea
ramesh banakar <ramesh.s.b@campusuvce.in>
2023-01-05 12:52:54 +0530

    array methods

--- 02_Arrays.js
+++ 02_Arrays.js
@@ -1,3 +1,15 @@
 const a=[1,2,34,"ramesh",null]
 console.log(a)
 console.log(a[4])
+
+a.push(10)
+a.pop()
+a.shift()
+a.unshift(30)
+a.splice(3,1,"javascript")
+console.log(a)
+
+
+for(let item of a){
+    console.log(item)
+}
