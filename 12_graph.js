9959cc123298e00fcff061f25b8bf1edf39c2f81
ramesh banakar <ramesh.s.b@campusuvce.in>
2023-01-12 21:16:44 +0530

    graph

---
+++ 12_graph.js
@@ -0,0 +1,13 @@
+//A graph is non-linear data structure that consist of infinite number of vertices conneted by edges
+//trees are specific type of data structure
+
+//directed graph-->in this edges have the direction
+//undirected graph-->in this edges do not have the direction
+
+//grpah usage
+//------------- 
+//google maps
+//social media
+
+
+
